c9ec301538897e71b243d102299e353ff54c8d69
dlandberg <DLandberg@sphereconsultinginc.com>
2010-10-08 00:45:58 -0700

    ---

--- lib/image.rb
+++ lib/image.rb
@@ -2,42 +2,45 @@ require "pdf/writer"
 require 'lib/base_concrete_region'
 
 module PDFRegion
-  class Image < BaseConcreteRegion
-    def initialize(parent)
-      super parent
+	class Image < BaseConcreteRegion
+		def initialize(parent)
+			super parent
 
-      @image = nil
-      @resize = nil
-      @border = nil
-      @link = nil      
-    end
+			@image = nil
+			#@resize = nil
+			@border = nil
+			@link = nil      
+		end
 
-    attr_accessor :image
-    attr_accessor :resize
-    attr_accessor :border     
-    
-    def render(x, y, test=false)
-      info = PDF::Writer::Graphics::ImageInfo.new(@image)
-      p width
-      p height
-      
-      width  ||= height / info.height.to_f * info.width
-      height ||= width * info.height / info.width.to_f
-      
-      p width
-      p height
-      document.pdf.add_image(@image, x, y - width, width,height)
-    end
-  end
+		attr_accessor :image
+		#attr_accessor :resize
+		#attr_accessor :border
+		attr_reader :link
+				
+		def render(x, y, test=false)
+			info = PDF::Writer::Graphics::ImageInfo.new(@image)
+			p width_new = width if width > 0
+			p height_new = height if height > 0
+			
+			width_new  ||= height / info.height.to_f * info.width
+			height_new ||= width * info.height / info.width.to_f
+			
+			p width_new
+			p height_new
+			document.pdf.add_image(@image, x, y - width_new, width_new,height_new) if (width_new > 0 && height_new > 0)
+			document.pdf.add_image(@image, x, y - info.width, nil,nil) if (width_new == 0 && height_new == 0)
+			add_border(x,y)
+		end
+	end
 
-  module ImageContainer
-    def image(image_res, style=nil, &initialization_block)
-      image = Image.new self
-      image.image = image_res
-      image.set_properties style unless style.nil?
-      image.instance_eval(&initialization_block) if initialization_block
+	module ImageContainer
+		def image(image_res, style=nil, &initialization_block)
+			image = Image.new self
+			image.image = image_res
+			image.set_properties style unless style.nil?
+			image.instance_eval(&initialization_block) if initialization_block
 
-      self.add_region(image)
-    end
-  end
+			self.add_region(image)
+		end
+	end
 end
--- test.rb
+++ test.rb
@@ -5,15 +5,15 @@ require "pdf/writer"
 require "lib/document"
 require "lib/fixnum"
 require "lib/float"
-require 'ruby-debug'
+#require 'ruby-debug'
 
 
 
 result = PDFRegion::document PDF::Writer.new, 2.cm do
-#  debugger
+  #debugger
   image_data = open(File.expand_path('../test_image.jpg'), "rb") { |file| file.read }
   
-  image image_data, :width => 200
+  image image_data, :width => 250, :height => 250, :border => 2.cm
   caption 'test text'*50, :width=>150, :pad_left=>0.5.cm, :text_color=>Color::RGB::Blue, :border => 1.cm
 
 
