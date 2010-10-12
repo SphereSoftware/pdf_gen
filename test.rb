7a881f49bf3014a1e79ca03af02ee2d292a8af9a
Vsizov <VSizov@sphereconsultinginc.com>
2010-10-12 13:44:49 +0300

    ---

--- test.rb
+++ test.rb
@@ -12,22 +12,10 @@ result = PDFRegion::document PDF::Writer.new, 2.cm do
   #debugger
 
   div do
-    caption "text" * 20, :width=>150
-    div do
       caption 'test text'*60, :width=>150, :pad_left=>0.5.cm, :text_color=>Color::RGB::Red, :justification => :right
       caption 'test text'*50, :width=>150, :pad_left=>0.5.cm, :text_color=>Color::RGB::Blue, :justification => :right
       caption 'test text'*60, :width=>150, :pad_left=>0.5.cm, :text_color=>Color::RGB::Red, :justification => :right
       caption 'test text'*60, :width=>150, :pad_left=>0.5.cm, :text_color=>Color::RGB::Red, :justification => :right
-    end
-
-#    span do
-#      image_data = open(File.expand_path('../test_image.jpg'), "rb") { |file| file.read }
-#
-#      image image_data, :width => 250, :border => 2
-#      caption 'test text'*50, :width=>150, :pad_left=>0.5.cm, :text_color=>Color::RGB::Blue, :justification => :right
-#      caption 'test text'*60, :width=>150, :pad_left=>0.5.cm, :text_color=>Color::RGB::Red, :justification => :right
-#      caption 'test text'*60, :width=>150, :pad_left=>0.5.cm, :text_color=>Color::RGB::Red, :justification => :center
-#    end
   end
 end
 
