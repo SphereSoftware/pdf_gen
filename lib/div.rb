acabcc238e3be12f1754adbdcd98f1ab99122d5f
Vsizov <VSizov@sphereconsultinginc.com>
2010-10-14 10:17:47 +0300

    format code

--- lib/div.rb
+++ lib/div.rb
@@ -26,7 +26,7 @@ module PDFRegion
 
     #gorizontal alignment
     attr_accessor :gorizontal_align
-    
+
     #optional border
     attr_accessor :optional_border
 
@@ -42,27 +42,27 @@ module PDFRegion
 
         unless test
           if (region.height >= (document.pdf.y - pad_bottom))
-            add_optional_border(x,document.pdf.y) if optional_border
+            add_optional_border(x, document.pdf.y) if optional_border
             document.break_page
           end
-          
+
           if (region.width > (width - pad_left - pad_right)) or gorizontal_align
             region.width = width - pad_left - pad_right
           end
           add_border_top(x, document.pdf.y) if region == first
-          
+
           document.pdf.y -= pad_top if region == first
           region.render(x + pad_left, document.pdf.y)
-          
-          y = region==first ? document.pdf.y + pad_top : document.pdf.y
-          y_new = region==last ?  document.pdf.y-region.height - pad_bottom : document.pdf.y-region.height
-          
-          add_border_sides(x,y,y_new) #left and right borders
-          
+
+          y = region == first ? document.pdf.y + pad_top : document.pdf.y
+          y_new = region == last ? document.pdf.y - region.height - pad_bottom : document.pdf.y - region.height
+
+          add_border_sides(x, y, y_new)
+
           document.pdf.y -= region.height
           document.pdf.y -= gorizontal_interval unless region == last
           document.pdf.y -= pad_bottom if region == last
-          
+
           add_border_bottom(x, document.pdf.y) if region == last
         end
 
@@ -75,34 +75,34 @@ module PDFRegion
 
     private :render_regions
 
-    def add_border_top(x,y)
+    def add_border_top(x, y)
       document.pdf.line(x, y, x + width, y).stroke if border_top
     end
-    
-    def add_border_bottom(x,y)
+
+    def add_border_bottom(x, y)
       document.pdf.line(x, y, x + width, y).stroke if border_bottom
     end
-    
-    def add_border_sides(x,y,y_new)
+
+    #left and right borders
+    def add_border_sides(x, y, y_new)
       document.pdf.line(x, y, x, y_new).stroke if border_left
       document.pdf.line(x + width, y, x + width, y_new).stroke if border_right
     end
-    
-    def add_optional_border(x,y)
+
+    def add_optional_border(x, y)
       border_style = PDF::Writer::StrokeStyle::DOTTED
       document.pdf.stroke_style! border_style
-      
+
       document.pdf.line(x, y, x + width, y).stroke
-      
+
       border_style = PDF::Writer::StrokeStyle::SOLID
       document.pdf.stroke_style! border_style
     end
-    #gets minimal region height
 
     def calculate_minimal_height
       0
     end
-    
+
     def render(x, y, test=false)
       super x, y, test
       [x, y]
