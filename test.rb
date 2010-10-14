f69571458bad5b60d0d9e31004fdf368e4dd05db
Vsizov <VSizov@sphereconsultinginc.com>
2010-10-14 18:19:46 +0300

    ---

--- test.rb
+++ test.rb
@@ -12,12 +12,13 @@ result = PDFRegion::document PDF::Writer.new, 2.cm do
 #  debugger
   div :width => 10.cm, :pad_left => 2.cm do
     div :pad_left=>2.cm, :pad_top => 2.cm, :pad_bottom=> 2.cm, :pad_right=>2.cm,  :width=> 8.cm, :border=> true, :optional_border => true do
-      caption 'test text'*50, :width=>5.cm, :height => 500
-      caption 'test text'*50, :width=>5.cm, :height => 500
+      caption 'test text'*50, :width=>5.cm
+      caption 'test text'*50, :width=>5.cm
+      caption 'test text'*50, :width=>5.cm
+      caption 'test text'*50, :width=>5.cm
     end
   
   end
-  
   
 end
 
