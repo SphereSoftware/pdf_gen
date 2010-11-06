5dcae128e81ae51c8ccf7b3b3c0fac3c6162c00e
Vsizov <VSizov@sphereconsultinginc.com>
2010-11-06 15:54:57 +0200

    fix table_container

--- lib/containers/table_container.rb
+++ lib/containers/table_container.rb
@@ -6,7 +6,8 @@ include PDFRegion
 module TableContainer
 
   def table(style = nil, &initialization_block)
-    table = style.has_key?(:data_source) ? SmartTable.new(self) : Table.new(self)
+    table = (style.kind_of?(Hash) and style.has_key?(:data_source)) ?\
+ SmartTable.new(self) : Table.new(self)
     table.width = self.width - self.pad_left - self.pad_right
     table.set_properties style unless style.nil?
     table.instance_eval(&initialization_block) if initialization_block
