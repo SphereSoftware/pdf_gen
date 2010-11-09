ba392d4d9fc94543afea996030c024628da13a12
Vsizov <VSizov@sphereconsultinginc.com>
2010-11-09 16:33:41 +0200

    fix image width

--- lib/containers/image_container.rb
+++ lib/containers/image_container.rb
@@ -1,14 +1,15 @@
-module PDFRegion
+require "lib/image"
+include PDFRegion
 
-  module ImageContainer
 
-    def image(image_res, style=nil, &initialization_block)
-      image = Image.new self, image_res
-      image.width = self.width - self.pad_left - self.pad_right
-      image.set_properties style unless style.nil?
-      image.instance_eval(&initialization_block) if initialization_block
-      self.add_region(image)
-    end
-
+module ImageContainer
+  
+  def image(image_res, style=nil, &initialization_block)
+    image = Image.new self, image_res
+#    image.width = self.width - self.pad_left - self.pad_right
+    image.set_properties style unless style.nil?
+    image.instance_eval(&initialization_block) if initialization_block
+    self.add_region(image)
   end
+  
 end
