8dbf5342adf4cf343c5968ab7b149c7383fefd21
Vsizov <VSizov@sphereconsultinginc.com>
2010-11-09 16:29:30 +0200

    new sample for table

--- samples/table.rb
+++ samples/table.rb
@@ -1,90 +1,37 @@
-$:.unshift(File.expand_path(File.dirname(__FILE__) + "/../"))
-require "document"
+require "../document"
 
+image_data = open(File.expand_path('samples/ruby_logo.jpg'), "rb") { |file| file.read }
 
-result = PDFRegion::document PDF::Writer.new, 0.cm do
-
-  table do
-    self.width = document.pdf.page_width
-    self.repeat_header_on_each_page = true
-    self.repeat_footer_on_each_page = false
-    title :horizontal_align => true do
-      caption "Transactions for Account Number",
-              :pad_top => 0.1.cm, :pad_bottom => 0.1.cm, :pad_left => 5,
-              :text_color => Color::RGB::Red, :border => true, :justification => :center
-    end
-
-    header :horizontal_align => true do
-
-      span do
-
-        caption "Reference Number",
-                :width => document.pdf.page_width / 2, :justification => :center,
-                :border_right => true, :border_left => true
-
-        caption "Tran Date",
-                :width => document.pdf.page_width / 2, :justification => :center,
-                :border_right => true, :border_left => true
+result = PDFRegion::document PDF::Writer.new, 2.cm do
+  div :paddings => 15 do
+    table do
+      title do
+        caption "<i>Table 1.1.</i> <b>Users</b>", :pad_bottom => 1
       end
-    end
-
-    body :horizontal_align => true do
-
-      span do
-
-        caption "Some Value"*100,
-                :width => document.pdf.page_width / 2, :justification => :center,
-                :border_right => true, :border_left => true, :pad_bottom => 2
-
-        caption "Some Value"*150,
-                :width => document.pdf.page_width / 2, :justification => :center,
-                :border_right => true, :border_left => true, :pad_bottom => 2
+      header do
+        span do
+          caption "name", :width => self.width/3
+          caption "Last name", :width => self.width/3
+          caption "Email", :width => self.width/3
+          elements :border => true, :paddings => 1, :justification => :center
+        end
       end
-      span do
-
-        caption "Some Value"*200,
-                :width => document.pdf.page_width / 2, :justification => :center,
-                :border_right => true, :border_left => true, :pad_bottom => 2
-
-        caption "Some Value"*250,
-                :width => document.pdf.page_width / 2, :justification => :center,
-                :border_right => true, :border_left => true, :pad_bottom => 2
-      end
-      span do
-
-        caption "Some Value"*190,
-                :width => document.pdf.page_width / 2, :justification => :center,
-                :border_right => true, :border_left => true, :pad_bottom => 2
-
-        caption "Some Value"*190,
-                :width => document.pdf.page_width / 2, :justification => :center,
-                :border_right => true, :border_left => true, :pad_bottom => 2
-      end
-      span do
-
-        caption "Some Value"*100,
-                :width => document.pdf.page_width / 2, :justification => :center,
-                :border_right => true, :border_left => true, :pad_bottom => 2
-
-        caption "Some Value"*110,
-                :width => document.pdf.page_width / 2, :justification => :center,
-                :border_right => true, :border_left => true, :pad_bottom => 2
-      end
-    end
-    footer :horizontal_align => true do
-
-      span do
-
-        caption "Footer1",
-                :width => document.pdf.page_width / 2, :justification => :center,
-                :border_right => true, :border_left => true, :pad_bottom => 2
-
-        caption "Footer2",
-                :width => document.pdf.page_width / 2, :justification => :center,
-                :border_right => true, :border_left => true, :pad_bottom => 2
+      body do
+        span do
+          caption "Valeriy", :width => self.width/3
+          caption "Sizov", :width => self.width/3
+          caption "example@gmail.com", :width => self.width/3
+          elements :border => true, :paddings => 1
+        end
+        span do
+          caption "Dmitriy", :width => self.width/3
+          caption "Landberg", :width => self.width/3
+          caption "example1@gmail.com", :width => self.width/3
+          elements :border => true, :paddings => 1
+        end
       end
     end
   end
 end
+File.open("#{File.basename(__FILE__, ".rb")}.pdf", 'wb') {|f| f.write result}
 
-File.open("#{File.dirname(__FILE__)}/../doc_test.pdf", 'wb') {|f| f.write result}
