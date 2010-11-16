761530b7770e25f2b22d0b9ab2db24c727da43e5
dlandberg <DLandberg@sphereconsultinginc.com>
2010-11-16 11:06:56 +0200

    small fix

--- lib/div.rb
+++ lib/div.rb
@@ -116,7 +116,6 @@ module PDFGen
     end
 
     def render(pos, av_height, test=false)
-      debugger
       pos_x, pos_y = pos
       add_border_top(pos_x, pos_y) if @rendered_height.zero?
       fill(pos)
